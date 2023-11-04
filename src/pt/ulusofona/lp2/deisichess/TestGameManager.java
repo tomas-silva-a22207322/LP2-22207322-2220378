6c24bf97209e98fa710ffd90742f40e6b0c0fb1a
Daniel Rodrigues <danielconceicaorodrigues23@hotmail.com>
2023-11-04 18:58:03 +0000

    created TestGameManager

---
+++ src/pt/ulusofona/lp2/deisichess/TestGameManager.java
@@ -0,0 +1,17 @@
+package pt.ulusofona.lp2.deisichess;
+
+import org.testng.annotations.Test;
+
+public class TestGameManager {
+    //A função de teste não pode estar vazia;
+    //A função de teste tem de ter pelo menos um assert;
+    //A função de teste tem de chamar código do projecto;
+    //O assert tem de comparar o resultado de uma função do projecto com um resultado
+    //esperado.
+
+    @Test
+    public void teste01() {
+
+    }
+
+}
