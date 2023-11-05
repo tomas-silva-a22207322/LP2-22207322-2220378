8c36298766a1cb541277279b25e163bb15be330a
Daniel Rodrigues <danielconceicaorodrigues23@hotmail.com>
2023-11-05 01:17:15 +0000

    created JUnit tests

--- src/pt/ulusofona/lp2/deisichess/TestGameManager.java
+++ src/pt/ulusofona/lp2/deisichess/TestGameManager.java
@@ -1,6 +1,9 @@
 package pt.ulusofona.lp2.deisichess;
 
 import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
+import static org.testng.internal.junit.ArrayAsserts.assertArrayEquals;
 
 public class TestGameManager {
     //A função de teste não pode estar vazia;
@@ -10,8 +13,78 @@ public class TestGameManager {
     //esperado.
 
     @Test
-    public void teste01() {
+    public void testGetPieceInfo() {
+        GameManager gameManager = new GameManager();
 
+        Tabuleiro tabuleiro = new Tabuleiro(8);
+        gameManager.setTabuleiro(tabuleiro);
+
+        Peca peca = new Peca(1, 0, 0, "Rei");
+        tabuleiro.getPecas().put(1, peca);
+        tabuleiro.setPecabyPosicao(0, 0, peca);
+
+
+        String[] resultadoAtual = gameManager.getPieceInfo(1);
+        String[] resultadoEsperado = {"1", "0", "0", "Rei", "capturado", "", ""};
+
+        assertArrayEquals(resultadoEsperado, resultadoAtual);
+    }
+
+    @Test
+    public void testGetPieceInfoAsString() {
+        GameManager gameManager = new GameManager();
+
+        Tabuleiro tabuleiro = new Tabuleiro(4);
+        gameManager.setTabuleiro(tabuleiro);
+
+        Peca peca = new Peca(1, 0, 0, "Rei");
+        peca.setCapturado(false);
+        tabuleiro.getPecas().put(1, peca);
+        tabuleiro.setPecabyPosicao(0, 0, peca);
+
+
+        String resultadoAtual = gameManager.getPieceInfoAsString(1);
+        String resultadoReal = "1 | 0 | 0 | Rei @ (0, 0)";
+
+        assertEquals(resultadoReal, resultadoAtual);
+    }
+
+    @Test
+    public void testGetSquareInfo() {
+        GameManager gameManager = new GameManager();
+
+        Tabuleiro tabuleiro = new Tabuleiro(6);
+        gameManager.setTabuleiro(tabuleiro);
+
+        Peca peca = new Peca(1, 0, 0, "Rei");
+        peca.setCapturado(false);
+        tabuleiro.getPecas().put(1, peca);
+        tabuleiro.setPecabyPosicao(0, 0, peca);
+
+        String[] result = gameManager.getSquareInfo(0, 0);
+
+        // Assert
+        String[] expected = {"1", "0", "0", "Rei", "crazy_emoji_black.png"};
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void testGameOverPecasEquipa0Is0() {
+        GameManager gameManager = new GameManager();
+
+        Tabuleiro tabuleiro = new Tabuleiro(2);
+        gameManager.setTabuleiro(tabuleiro);
+
+        Peca peca = new Peca(1, 0, 1, "Rei");
+        peca.setCapturado(false);
+        tabuleiro.getPecas().put(1, peca);
+        tabuleiro.setPecabyPosicao(0, 0, peca);
+        // Act
+        boolean resultado = gameManager.gameOver();
+
+        // Assert
+        assertTrue(resultado); // The game should be over.
+        assertEquals("VENCERAM AS BRANCAS", gameManager.getResultado());
     }
 
 }
