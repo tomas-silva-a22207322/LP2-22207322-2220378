f831ba2b64772a13b34a0b10341aff513da4733d
Daniel Rodrigues <danielconceicaorodrigues23@hotmail.com>
2023-11-25 18:28:56 +0000

    created new pecas subclasses

---
+++ src/pt/ulusofona/lp2/deisichess/Rei.java
@@ -0,0 +1,9 @@
+package pt.ulusofona.lp2.deisichess;
+
+public class Rei extends Peca{
+
+
+    public Rei(int id, int tipo, int equipa, String nome) {
+        super(id, tipo, equipa, nome);
+    }
+}
---
+++ src/pt/ulusofona/lp2/deisichess/TorreVertical.java
@@ -0,0 +1,7 @@
+package pt.ulusofona.lp2.deisichess;
+
+public class TorreVertical extends Peca{
+    public TorreVertical(int id, int tipo, int equipa, String nome) {
+        super(id, tipo, equipa, nome);
+    }
+}
