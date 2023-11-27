0bf4a440efe027d43f8a86bf5458322aee0a66f1
Daniel Rodrigues <danielconceicaorodrigues23@hotmail.com>
2023-11-27 22:10:44 +0000

    changed joker isValidMove

--- src/pt/ulusofona/lp2/deisichess/Joker.java
+++ src/pt/ulusofona/lp2/deisichess/Joker.java
@@ -9,31 +9,28 @@ public class Joker extends Peca{
 
     @Override
     public boolean isValidMove(int x0, int y0, int x1, int y1, int turno) {
-        int movePeca = turno % 7;
+        int movePeca = turno % 6;
 
         Peca peca;
 
         switch (movePeca) {
-            case 1: // Rainha
+            case 0: // Rainha
                 peca = new Rainha(getId(),  getTipo(),  getEquipa(),  getNome());
                 return peca.isValidMove(x0, y0, x1, y1, turno);
-            case 2: // Ponei Mágico
+            case 1: // Ponei Mágico
                 peca = new PoneiMagico(getId(),  getTipo(),  getEquipa(),  getNome());
                 return peca.isValidMove(x0, y0, x1, y1, turno);
-            case 3: // Padre da Vila
+            case 2: // Padre da Vila
                 peca = new PadreDaVila(getId(),  getTipo(),  getEquipa(),  getNome());
                 return peca.isValidMove(x0, y0, x1, y1, turno);
-            case 4: // Torre Horizontal
+            case 3: // Torre Horizontal
                 peca = new TorreHorizontal(getId(),  getTipo(),  getEquipa(),  getNome());
                 return peca.isValidMove(x0, y0, x1, y1, turno);
-            case 5: // Torre Vertical
+            case 4: // Torre Vertical
                 peca = new TorreVertical(getId(),  getTipo(),  getEquipa(),  getNome());
                 return peca.isValidMove(x0, y0, x1, y1, turno);
-            case 6: // Homer Simpson
+            case 5: // Homer Simpson
                 peca = new HomerSimpson(getId(),  getTipo(),  getEquipa(),  getNome());
-                return peca.isValidMove(x0, y0, x1, y1, turno);
-            case 0: // Rei
-                peca = new Rei(getId(),  getTipo(),  getEquipa(),  getNome());
                 return peca.isValidMove(x0, y0, x1, y1, turno);
             default:
                 return false;
