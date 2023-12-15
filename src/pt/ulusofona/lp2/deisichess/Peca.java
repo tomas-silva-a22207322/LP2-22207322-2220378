f5c6ec78d20bb287790c4591ba5a5bd4d89c84f3
daniel-rodrigues-a22203178 <148213151+daniel-rodrigues-a22203178@users.noreply.github.com>
2023-12-15 23:49:05 +0000

    Update Peca.java
    isValidMove now receives tabuleiro has a parameter

--- src/pt/ulusofona/lp2/deisichess/Peca.java
+++ src/pt/ulusofona/lp2/deisichess/Peca.java
@@ -21,7 +21,7 @@ public abstract class Peca {
 
 
 
-    public abstract boolean isValidMove(int x0, int y0, int x1, int y1, int turno);
+    public abstract boolean isValidMove(int x0, int y0, int x1, int y1, int turno, Tabuleiro tabuleiro);
 
     //GETTERS
     public int getId() {return id;}
