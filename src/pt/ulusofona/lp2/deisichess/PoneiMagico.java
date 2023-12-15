7b29b250f546fe6af86fe351f5c28020afcdb623
daniel-rodrigues-a22203178 <148213151+daniel-rodrigues-a22203178@users.noreply.github.com>
2023-12-15 23:53:02 +0000

    Update PoneiMagico.java
    Updated isValidMove of PoneiMagico

--- src/pt/ulusofona/lp2/deisichess/PoneiMagico.java
+++ src/pt/ulusofona/lp2/deisichess/PoneiMagico.java
@@ -2,17 +2,46 @@ package pt.ulusofona.lp2.deisichess;
 
 public class PoneiMagico extends Peca{
 
-    int pontuacao = 5;
 
     public PoneiMagico(int id, int tipo, int equipa, String nome) {
         super(id, tipo, equipa, nome);
     }
 
     @Override
-    public boolean isValidMove(int x0, int y0, int x1, int y1, int turno) {
+    public boolean isValidMove(int x0, int y0, int x1, int y1, int turno, Tabuleiro tabuleiro) {
         int dx = Math.abs(x1 - x0);
         int dy = Math.abs(y1 - y0);
 
-        return (dx == 2 && dy == 2);
+        // Verificação do caminho no eixo x primeiro e depois no eixo y
+        boolean path1Valid = isPathValid(x0, y0, x1, y0, x1, y1, tabuleiro);
+
+        // Verificação do caminho no eixo y primeiro e depois no eixo x
+        boolean path2Valid = isPathValid(x0, y0, x0, y1, x1, y1, tabuleiro);
+
+        return path1Valid || path2Valid;
+    }
+
+    // Método para verificar se o caminho entre duas posições é válido
+    private boolean isPathValid(int x0, int y0, int x1, int y1, int xFinal, int yFinal, Tabuleiro tabuleiro) {
+        int compareX = Integer.compare(x1, x0);
+        int compareY = Integer.compare(y1, y0);
+
+        int x = x0 + compareX;
+        int y = y0 + compareY;
+
+        while (x != xFinal || y != yFinal) {
+            if (tabuleiro.getPecabyPosicao(x, y) != null) {
+                return false;
+            }
+
+            if (x != xFinal) {
+                x += compareX;
+            }
+            if (y != yFinal) {
+                y += compareY;
+            }
+        }
+
+        return true;
     }
 }
