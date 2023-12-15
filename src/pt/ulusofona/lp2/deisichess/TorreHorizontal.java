f1e204245f506044723fdf2ceb53d1df619cfaec
daniel-rodrigues-a22203178 <148213151+daniel-rodrigues-a22203178@users.noreply.github.com>
2023-12-15 23:54:24 +0000

    Update TorreHorizontal.java
    Updated isValidMove of TorreHorizontal

--- src/pt/ulusofona/lp2/deisichess/TorreHorizontal.java
+++ src/pt/ulusofona/lp2/deisichess/TorreHorizontal.java
@@ -8,7 +8,18 @@ public class TorreHorizontal extends Peca{
     }
 
     @Override
-    public boolean isValidMove(int x0, int y0, int x1, int y1, int turno) {
-        return (y0 == y1);
+    public boolean isValidMove(int x0, int y0, int x1, int y1, int turno, Tabuleiro tabuleiro) {
+        if (y0 == y1){
+            int minX = Math.min(x0, x1);
+            int maxX = Math.max(x0, x1);
+
+            for (int x = minX + 1; x < maxX; x++) {
+                if (tabuleiro.getPecabyPosicao(x, y0) != null) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
     }
 }
