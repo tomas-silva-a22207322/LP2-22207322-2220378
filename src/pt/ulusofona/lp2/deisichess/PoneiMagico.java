d792e7380999732efe5f1cb9f6f101d8495cb470
daniel-rodrigues-a22203178 <148213151+daniel-rodrigues-a22203178@users.noreply.github.com>
2023-12-19 18:12:05 +0000

    fixed ponei magico path checking

--- src/pt/ulusofona/lp2/deisichess/PoneiMagico.java
+++ src/pt/ulusofona/lp2/deisichess/PoneiMagico.java
@@ -13,35 +13,48 @@ public class PoneiMagico extends Peca{
         int dy = Math.abs(y1 - y0);
 
         // Verificação do caminho no eixo x primeiro e depois no eixo y
-        boolean path1Valid = isPathValid(x0, y0, x1, y0, x1, y1, tabuleiro);
+        boolean pathyxValid = isPathValid(x0, y0, x1, y0, tabuleiro, "eixoY eixoX");
 
         // Verificação do caminho no eixo y primeiro e depois no eixo x
-        boolean path2Valid = isPathValid(x0, y0, x0, y1, x1, y1, tabuleiro);
+        boolean pathxyValid = isPathValid(x0, y0, x0, y1, tabuleiro, "eixoX eixoY");
 
-        return path1Valid || path2Valid;
+        return pathyxValid || pathxyValid;
     }
 
-    // Método para verificar se o caminho entre duas posições é válido
-    private boolean isPathValid(int x0, int y0, int x1, int y1, int xFinal, int yFinal, Tabuleiro tabuleiro) {
+    private boolean isPathValid(int x0, int y0, int x1, int y1, Tabuleiro tabuleiro, String direcao) {
+        
         int compareX = Integer.compare(x1, x0);
         int compareY = Integer.compare(y1, y0);
-
-        int x = x0 + compareX;
-        int y = y0 + compareY;
-
-        while (x != xFinal || y != yFinal) {
-            if (tabuleiro.getPecabyPosicao(x, y) != null) {
-                return false;
-            }
-
-            if (x != xFinal) {
+        
+        int x;
+        int y;
+        
+        if(Objects.equals(direcao, "eixoX eixoY")){
+            x = x0 + compareX;
+            y = y0;
+            
+            while(x != x0){
+                if (tabuleiro.getPecabyPosicao(x, y) != null) {
+                    return false;
+                }
                 x += compareX;
             }
-            if (y != yFinal) {
+
+            return tabuleiro.getPecabyPosicao(x, y+compareY) != null;
+        }
+        if(Objects.equals(direcao, "eixoY eixoX")){
+            x = x0;
+            y = y0 + compareY;
+
+            while(y != y0){
+                if (tabuleiro.getPecabyPosicao(x, y) != null) {
+                    return false;
+                }
                 y += compareY;
             }
-        }
 
-        return true;
+            return tabuleiro.getPecabyPosicao(x + compareX, y) != null;
+        }
+        return false;
     }
 }
