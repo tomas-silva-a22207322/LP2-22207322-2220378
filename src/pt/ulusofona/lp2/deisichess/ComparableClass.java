2e311bbed94ac57b0646aaad269d9ff7216d48e2
Daniel Rodrigues <danielconceicaorodrigues23@hotmail.com>
2023-12-20 01:20:38 +0000

    created ComparableClass

---
+++ src/pt/ulusofona/lp2/deisichess/ComparableClass.java
@@ -0,0 +1,42 @@
+package pt.ulusofona.lp2.deisichess;
+
+public class ComparableClass<T extends Comparable<T>> implements Comparable<ComparableClass<T>> {
+    int x;
+    int y;
+    int pontuacao;
+
+    public ComparableClass(int x, int y, int pontuacao) {
+        this.x = x;
+        this.y = y;
+        this.pontuacao = pontuacao;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+    @Override
+    public int compareTo(ComparableClass move){
+        return Integer.compare(move.pontuacao, getPontuacao());
+    }
+}
