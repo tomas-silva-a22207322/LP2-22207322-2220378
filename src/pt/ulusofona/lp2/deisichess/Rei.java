74abbf05af816ed68920d82fccc060aeee4ef998
Daniel Rodrigues <danielconceicaorodrigues23@hotmail.com>
2023-12-29 03:36:55 +0000

    removed pontuacao variable outside from constructor

--- src/pt/ulusofona/lp2/deisichess/Rei.java
+++ src/pt/ulusofona/lp2/deisichess/Rei.java
@@ -1,7 +1,6 @@
 package pt.ulusofona.lp2.deisichess;
 
 public class Rei extends Peca{
-    int pontuacao = 1000;
     public Rei(int id, int tipo, int equipa, String nome) {
         super(id, tipo, equipa, nome);
         pontuacao = 1000;
