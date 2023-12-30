b3f049b7577ae08ae560fb6384422e687a2c52e8
Daniel Rodrigues <danielconceicaorodrigues23@hotmail.com>
2023-12-30 02:53:48 +0000

    updated GameInfo, new variables, created undo and addMove

--- src/pt/ulusofona/lp2/deisichess/GameInfo.java
+++ src/pt/ulusofona/lp2/deisichess/GameInfo.java
@@ -3,27 +3,36 @@ package pt.ulusofona.lp2.deisichess;
 import java.util.ArrayList;
 
 public class GameInfo {
-    private ArrayList<Tabuleiro> estadosAnteriores;
-    private Tabuleiro estadoAtual;
+    private ArrayList<Tabuleiro> tabuleiros;
+    private Tabuleiro tabuleiroInicial;
+    private ArrayList<String> movimentos;
 
-    public GameInfo() {
-        estadosAnteriores = new ArrayList<>();
-        estadoAtual = null;
+    public GameInfo(Tabuleiro tabuleiroInicial) {
+        Tabuleiro novoTabuleiro = tabuleiroInicial.copy();
+        this.tabuleiroInicial = novoTabuleiro;
+        tabuleiros = new ArrayList<>();
+        tabuleiros.add(novoTabuleiro);
+        movimentos = new ArrayList<>();
     }
-//
-    public ArrayList<Tabuleiro> getEstadosAnteriores() {
-        return estadosAnteriores;
+    public Tabuleiro undo(){
+        this.tabuleiros.remove(tabuleiros.size() - 1);
+        this.movimentos.remove(movimentos.size() - 1);
+
+        return tabuleiros.get(tabuleiros.size() - 1).copy();
+    }
+    public void addMove(String move, Tabuleiro tabuleiro) {
+        this.tabuleiros.add(tabuleiro);
+        this.movimentos.add(move);
     }
 
-    public void setEstadosAnteriores(ArrayList<Tabuleiro> estadosAnteriores) {
-        this.estadosAnteriores = estadosAnteriores;
+    public ArrayList<Tabuleiro> getTabuleiros() {
+        return tabuleiros;
     }
-
-    public Tabuleiro getEstadoAtual() {
-        return estadoAtual;
+    public void setTabuleiros(ArrayList<Tabuleiro> Tabuleiros) {this.tabuleiros = Tabuleiros;}
+    public Tabuleiro TabuleiroInicial() {
+        return tabuleiroInicial;
     }
-
-    public void setEstadoAtual(Tabuleiro estadoAtual) {
-        this.estadoAtual = estadoAtual;
+    public void setTabuleiroInicial(Tabuleiro TabuleiroInicial) {
+        this.tabuleiroInicial = TabuleiroInicial;
     }
 }
